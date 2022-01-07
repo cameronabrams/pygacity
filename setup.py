f262232230074cdc3cf12c2600dfcc404b3f3c83
Cameron Abrams <cfa22@drexel.edu>
2022-01-07 11:55:17 -0500

    first

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='ThermoProblems',
+      version='0.1',
+      description='Routines for solving problems in thermodynamics',
+      url='github.com/AbramsTeaching/ThermoProblems',
+      author='Cameron F. Abrams',
+      author_email='cfa22@drexel.edu',
+      license='MIT',
+      packages=['Chem'],
+      zip_safe=False)
