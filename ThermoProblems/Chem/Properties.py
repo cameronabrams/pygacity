0291b1d29a187ff0fc9714a63c791695ce24f28f
Cameron Abrams <cfa22@drexel.edu>
2022-02-18 14:03:59 -0500

    update

--- ThermoProblems/Chem/Properties.py
+++ ThermoProblems/Chem/Properties.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import pkgutil
-from ThermoProblems.Chem.Compound import Compound
+from .Compound import Compound
 class PureProperties:
     ''' simple class for handling Sandler's pure properties database '''
     def __init__(self,inputfile='properties_binaries_database.xlsx',sheet_name='pure_properties'):
