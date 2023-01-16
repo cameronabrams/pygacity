79a7e8448a3717ce8c52d9837a035325ac3d5b8c
Cameron Abrams <cfa22@drexel.edu>
2023-01-15 21:21:25 -0500

    steamy steam

--- Sandlersteam/Sandlersteam.py
+++ Sandlersteam/Sandlersteam.py
@@ -31,11 +31,11 @@ class SATD:
             D.append(ndf)
         self.DF['P']=pd.concat(D,axis=0)
         self.DF['P'].sort_values(by='P',inplace=True)
-        print(self.DF['P'].head())
-        print(self.DF['P'].tail())
+        # print(self.DF['P'].head())
+        # print(self.DF['P'].tail())
         # print(self.DF['P'].info())
         self.lim['P']=[self.DF['P']['P'].min(),self.DF['P']['P'].max()]
-        print(self.lim)
+        # print(self.lim)
         D=[]
         for f,pu in zip(tfn,punits):
             data_abs_path=os.path.join(inst_root,'data',f)
@@ -105,6 +105,8 @@ class SUPH:
         hder=lines[0].split()
         # identify lines with pressures
         plines=[]
+        # save list of pressure values
+        Pval=[]
         for i,l in enumerate(lines):
             if 'P = ' in l:
                 plines.append(i)
@@ -127,7 +129,7 @@ class SUPH:
                     P.append(float(x))
             while len(Tsat)<len(P):
                 Tsat.append(None)
-
+            Pval.extend(P)
             data='\n'.join(lines[l+1:r])
             ndf=my_split(data,hder,P,Tsat,fixw=(phase=='L'))
             DFS.append(ndf)
@@ -146,15 +148,20 @@ class SUPH:
                 P.append(float(x))
         while len(Tsat)<len(P):
             Tsat.append(None)
+        Pval.extend(P)
         data='\n'.join(lines[plines[-1]+1:])
         ndf=my_split(data,hder,P,Tsat,fixw=(phase=='L'))
         DFS.append(ndf)
         self.data=pd.concat(DFS,axis=0)
         # print(f'Superheated steam table: {self.data["T"].min()}<T(C)<{self.data["T"].max()} {self.data["P"].min()}<P(MPa)<{self.data["P"].max()} ')
         dof=self.data.columns
+        # print(dof)
         self.uniqs={}
         for d in dof:
-            self.uniqs[d]=list(set(self.data[d].to_list())).sort()
+            self.uniqs[d]=np.sort(np.array(list(set(self.data[d].to_list()))))
+        #     print(f'{d}')
+        #     print(f'{self.uniqs[d]}')
+        # exit()
         self.interpolators={}
         for i in range(len(dof)):
             X=np.array(self.data[dof[i]].to_list())
@@ -169,6 +176,168 @@ class SUPH:
                         I=LinearNDInterpolator(list(zip(X,Y)),Z)
                         self.interpolators[f'{dof[i]}{dof[j]}'][dof[k]]=I
         # print(self.interpolators)
+    def TPBilinear(self,specdict):
+        retdict={}
+        xn,yn=specdict.keys()
+        assert [xn,yn]==['T','P']
+        xi,yi=specdict.values()
+        df=self.data
+        dof=self.data.columns
+        retdict={}
+        retdict['T']=xi
+        retdict['P']=yi
+        tdf=df[df['P']==yi]
+        if not tdf.empty:
+            # print(f'Found SH block for P = {yi}')
+            X=np.array(tdf['T'])
+            for d in dof:
+                if d not in 'TP':
+                    Y=np.array(tdf[d])
+                    retdict[d]=np.interp(xi,X,Y)
+        else:
+            for PL,PR in zip(self.uniqs['P'][:-1],self.uniqs['P'][1:]):
+                if PL<yi<PR:
+                    # print(f'Found {PL} < {yi} < {PR}')
+                    break
+            else:
+                raise Exception(f'P {yi} not between {PL} and {PR}')
+            X=np.array([PL,PR])
+            LDF=df[df['P']==PL]
+            RDF=df[df['P']==PR]
+            LT=np.array(LDF['T'])
+            RT=np.array(RDF['T'])
+            # print(f'LT {LT}')
+            # print(f'RT {RT}')
+            CT=np.array([T for T in LT if T in RT])
+            # print(f'Common T {CT}')
+            if xi in CT:
+                # print(f'Found line(s) for T {xi}')
+                for d in dof:
+                    if d not in 'TP':
+                        Y=np.array([LDF[LT==xi][d].values[0],RDF[RT==xi][d].values[0]])
+                        # print(f'X {X} Y {Y} yi {yi}')
+                        retdict[d]=np.interp(yi,X,Y)
+            else:
+                for TL,TR in zip(CT[:-1],CT[1:]):
+                    if TL<xi<TR:
+                        # print(f'Found {TL} < {xi} < {TR}')
+                        break
+                else:
+                    raise Exception(f'T {xi} not between {TL} and {TR}')
+                LTDF=LDF[(LDF['T']==TL)|(LDF['T']==TR)].sort_values(by='T')
+                RTDF=RDF[(RDF['T']==TL)|(RDF['T']==TR)].sort_values(by='T')
+                # print(LTDF.to_string())
+                # print(RTDF.to_string())
+                iv=np.zeros(2)
+                for p in dof:
+                    if not p in 'TP':
+                        for i in range(2):
+                            Lp=LTDF[p].values[i]
+                            Rp=RTDF[p].values[i]
+                            Y=np.array([Lp,Rp])
+                            # print(f'{p} {Lp} {Rp}')
+                            iv[i]=np.interp(yi,X,Y)
+                        retdict[p]=np.interp(xi,np.array([TL,TR]),iv)
+        return retdict
+
+
+    def TThBilinear(self,specdict):
+        xn,yn=specdict.keys()
+        assert xn=='T'
+        xi,yi=specdict.values()
+        df=self.data
+        dof=self.data.columns
+        LLdat={}
+        for d in dof:
+            if d!='T':
+                LLdat[d]=[]
+        for P in self.uniqs['P']:
+            tdf=df[df['P']==P]
+            X=np.array(tdf['T'])
+            Y=np.array(tdf[yn])
+            if Y.min()<yi<Y.max():
+                LLdat['P'].append(P)
+                for d in 'UVSH':
+                    Y=np.array(tdf[d])
+                    LLdat[d].append(np.interp(xi,X,Y))
+        X=LLdat[yn]
+        retdict={}
+        retdict['T']=xi
+        retdict[yn]=yi
+        for d in dof:
+            if d!='T' and d!=yn:
+                retdict[d]=np.interp(yi,X,LLdat[d])
+        # print(f'TThBilinear ret {retdict}')
+        return retdict
+
+    def PThBilinear(self,specdict):
+        xn,yn=specdict.keys()
+        assert xn=='P'
+        xi,yi=specdict.values()
+        df=self.data
+        dof=self.data.columns
+        LLdat={}
+        for d in dof:
+            if d!='P':
+                LLdat[d]=[]
+        for T in self.uniqs['T']:
+            tdf=df[df['T']==T]
+            X=np.array(tdf['P'])
+            Y=np.array(tdf[yn])
+            if Y.min()<yi<Y.max():
+                LLdat['T'].append(T)
+                for d in 'UVSH':
+                    Y=np.array(tdf[d])
+                    LLdat[d].append(np.interp(xi,X,Y))
+        X=LLdat[yn]
+        retdict={}
+        retdict['P']=xi
+        retdict[yn]=yi
+        for d in dof:
+            if d!='P' and d!=yn:
+                # print(f'Interp at {yn}={yi} from {d}={LLdat[d]} vs {yn}={X}')
+                retdict[d]=np.interp(yi,X,LLdat[d])
+        # print(f'PThBilinear ret {retdict}')
+        return retdict
+    
+    def ThThBilinear(self,specdict):
+        xn,yn=specdict.keys()
+        assert xn!='T' and yn!='T' and xn!='P' and yn!='P'
+        xi,yi=specdict.values()
+        df=self.data
+        dof=self.data.columns
+        LLdat={}
+        for d in dof:
+            if d not in 'TP':
+                LLdat[d]=[]
+        LLdat['T']=self.uniqs['T']
+        for T in LLdat['T']:
+            tdf=df[df['T']==T]
+            X=np.array(tdf[xn])
+            for d in dof:
+                if d!='T' and d!=xn:
+                    Y=np.array(tdf[d])
+                    if Y.min()<yi<Y.max():
+                        LLdat[d]=np.interp(xi,X,Y)
+        X=LLdat[yn]
+        retdict={}
+        retdict[xn]=xi
+        retdict[yn]=yi
+        for d in dof:
+            if d!=xn and d!=yn:
+                retdict[d]=np.interp(yi,X,LLdat[d])
+        return retdict
+
+    def Bilinear(self,specdict):
+        xn,yn=specdict.keys()
+        if [xn,yn]==['T','P']:
+            return self.TPBilinear(specdict)
+        elif xn=='T':
+            return self.TThBilinear(specdict)
+        elif xn=='P':
+            return self.PThBilinear(specdict)
+        else:
+            return self.ThThBilinear(specdict)
 
 _SATD=SATD()
 _SUPH=SUPH('V')
@@ -180,36 +349,38 @@ class PHASE:
 
 LARGE=1.e99
 NEGLARGE=-LARGE
-def MyBilinear(specdict,zn,scsh='SUPH'):
-    xn,yn=specdict.keys()
-    xi,yi=specdict.values()
-    if scsh=='SUPH':
-        df=_SUPH.data
-    elif scsh=='SUBC':
-        df=_SUBC.data
-    else:
-        raise Exception('MyBilinear only works on superheated or subcooled data')
-    df1=df[(df[xn]<xi)]
-    x0=df1[xn].max()
-    df1=df1[df1[xn]==x0].sort_values(by=yn)
-    df2=df[(df[xn]>xi)]
-    x1=df2[xn].min()
-    df2=df2[df2[xn]==x1].sort_values(by=yn)
-    df3=df[(df[yn]<yi)]
-    y0=df3[yn].max()
-    df3=df3[df3[yn]==y0].sort_values(by=xn)
-    df4=df[(df[yn]>yi)]
-    y1=df4[yn].min()
-    df4=df4[df4[yn]==y1].sort_values(by=xn)
-    x0y0=df[(df[xn]==x0)&(df[yn]==y0)]
-    x0y1=df[(df[xn]==x0)&(df[yn]==y1)]
-    x1y0=df[(df[xn]==x1)&(df[yn]==y0)]
-    x1y1=df[(df[xn]==x1)&(df[yn]==y1)]
-    inter=pd.concat((x0y0,x0y1,x1y0,x1y1),axis=0)
-    print(inter.to_string())
-    f=interp2d(np.array(inter[xn]),np.array(inter[yn]),np.array(inter[zn]))
-    z=f(xi,yi)
-    return z[0]
+
+
+# def MyBilinear(specdict,zn,scsh='SUPH'):
+#     xn,yn=specdict.keys()
+#     xi,yi=specdict.values()
+#     if scsh=='SUPH':
+#         df=_SUPH.data
+#     elif scsh=='SUBC':
+#         df=_SUBC.data
+#     else:
+#         raise Exception('MyBilinear only works on superheated or subcooled data')
+#     df1=df[(df[xn]<xi)]
+#     x0=df1[xn].max()
+#     df1=df1[df1[xn]==x0].sort_values(by=yn)
+#     df2=df[(df[xn]>xi)]
+#     x1=df2[xn].min()
+#     df2=df2[df2[xn]==x1].sort_values(by=yn)
+#     df3=df[(df[yn]<yi)]
+#     y0=df3[yn].max()
+#     df3=df3[df3[yn]==y0].sort_values(by=xn)
+#     df4=df[(df[yn]>yi)]
+#     y1=df4[yn].min()
+#     df4=df4[df4[yn]==y1].sort_values(by=xn)
+#     x0y0=df[(df[xn]==x0)&(df[yn]==y0)]
+#     x0y1=df[(df[xn]==x0)&(df[yn]==y1)]
+#     x1y0=df[(df[xn]==x1)&(df[yn]==y0)]
+#     x1y1=df[(df[xn]==x1)&(df[yn]==y1)]
+#     inter=pd.concat((x0y0,x0y1,x1y0,x1y1),axis=0)
+#     print(inter.to_string())
+#     f=interp2d(np.array(inter[xn]),np.array(inter[yn]),np.array(inter[zn]))
+#     z=f(xi,yi)
+#     return z[0]
 
 class SANDLER:
     _p=['T','P','u','v','s','h','x']
@@ -237,14 +408,14 @@ class SANDLER:
         p=self.spec[0]
         cp='P' if p=='T' else 'T'
         th=self.spec[1]
-        print(f'{p} = {self.__dict__[p]} ({self.satd.lim[p][0]}-{self.satd.lim[p][1]}), {th} = {self.__dict__[th]}')
+        # print(f'{p} = {self.__dict__[p]} ({self.satd.lim[p][0]}-{self.satd.lim[p][1]}), {th} = {self.__dict__[th]}')
         if self.satd.lim[p][0]<self.__dict__[p]<self.satd.lim[p][1]:
             ''' T or P is between saturation limits; may be a saturated state, so 
                 check whether the second property value lies between its liquid
                 and vapor phase values at this T or P '''
             thL=self.satd.interpolators[p][f'{th.upper()}L'](self.__dict__[p])
             thV=self.satd.interpolators[p][f'{th.upper()}V'](self.__dict__[p])
-            print(f'{th}L = {thL}, {th}V = {thV}')
+            # print(f'{th}L = {thL}, {th}V = {thV}')
             self.__dict__[cp]=self.satd.interpolators[p][cp](self.__dict__[p])
             if thL<self.__dict__[th]<thV:
                 ''' This is a saturated state! Use lever rule to get vapor fraction: '''
@@ -264,34 +435,37 @@ class SANDLER:
                         self.__dict__[pp]=self.x*self.Vapor.__dict__[pp]+(1-self.x)*self.Liquid.__dict__[pp]
             else:
                 ''' even though T or P is between saturation limits, the other property is not '''
+                specdict={p.upper():self.__dict__[p] for p in self.spec}
                 if self.__dict__[th]<thL:
                     ''' Th is below its liquid-state value; assume this is a subcooled state '''
-                    icode=''.join([x.upper() for x in self.spec])
-                    dofv=[self.__dict__[p] for p in self.spec]
+                    # icode=''.join([x.upper() for x in self.spec])
+                    # dofv=[self.__dict__[p] for p in self.spec]
+                    retdict=self.subc.Bilinear(specdict)
                     for p in self._p: 
-                        if p not in self.spec:
-                            self.__dict__[p]=self.subc.interpolators[icode][p.upper()](*dofv)
+                        if p not in self.spec and p!='x':
+                            self.__dict__[p]=retdict[p.upper()]
                 else:
                     ''' Th is above its vapor-state value; assume this is a superheated state '''
-                    icode=''.join([x.upper() for x in self.spec])
-                    dofv=[self.__dict__[p] for p in self.spec]
+                    retdict=self.suph.Bilinear(specdict)
                     for p in self._p: 
-                        if p not in self.spec:
-                            self.__dict__[p]=self.suph.interpolators[icode][p.upper()](*dofv)
+                        if p not in self.spec and p!='x':
+                            self.__dict__[p]=retdict[p.upper()]
         elif self.__dict__[p]>self.satd.lim[p][1]:
             ''' Th is above its vapor-state value; assume this is a superheated state '''
-            icode=''.join([x.upper() for x in self.spec])
-            dofv=[self.__dict__[p] for p in self.spec]
+            # icode=''.join([x.upper() for x in self.spec])
+            # dofv=[self.__dict__[p] for p in self.spec]
+            specdict={p.upper():self.__dict__[p] for p in self.spec}
+            retdict=self.suph.Bilinear(specdict)
             for p in self._p: 
                 if p not in self.spec and p!='x':
-                    self.__dict__[p]=self.suph.interpolators[icode][p.upper()](*dofv)
+                    self.__dict__[p]=retdict[p.upper()]
         else:
             ''' Th is below its liquid-state value; assume this is a subcooled state '''
-            icode=''.join([x.upper() for x in self.spec])
-            dofv=[self.__dict__[p] for p in self.spec]
+            specdict={p.upper():self.__dict__[p] for p in self.spec}
+            retdict=self.subc.Bilinear(specdict)
             for p in self._p: 
                 if p not in self.spec and p!='x':
-                    self.__dict__[p]=self.subc.interpolators[icode][p.upper()](*dofv)
+                    self.__dict__[p]=retdict[p.upper()]
 
     def _resolve_subsup(self):
         ''' T and P are both given explicitly.  Could be either superheated or subcooled state '''
@@ -303,14 +477,13 @@ class SANDLER:
             Psat=LARGE
         if self.P>Psat:
             ''' P is higher than saturation: this is a subcooled state '''
-            for p in self._p: 
-                if p not in self.spec and p!='x':
-                    self.__dict__[p]=MyBilinear(specdict,p.upper(),scsh='SUBC')
+            retdict=self.subc.Bilinear(specdict)
         else:
             ''' P is lower than saturation: this is a superheated state '''
-            for p in self._p: 
-                if p not in self.spec and p!='x':
-                    self.__dict__[p]=MyBilinear(specdict,p.upper(),scsh='SUPH')
+            retdict=self.suph.Bilinear(specdict)
+        for p in self._p: 
+            if p not in self.spec and p!='x':
+                self.__dict__[p]=retdict[p.upper()]
 
     def _resolve_satd(self):
         ''' This is an explicitly saturated state with vapor fraction (x) and one 
