83cc17ec00d10a89f475e81ad86b1adc6a345ef3
Cameron Abrams <cfa22@drexel.edu>
2025-01-03 12:16:49 -0500

    build

--- tests/unit/test_build.py
+++ tests/unit/test_build.py
@@ -11,3 +11,14 @@ class BuildTests(unittest.TestCase):
         c.document.resolve_instance(keymap)
         self.assertTrue(os.path.exists('exam-12345678.tex'))
         c.LB.build_document(c.document,make_solutions=True)
+        self.assertTrue(os.path.exists('exam-12345678.pdf'))
+        self.assertTrue(os.path.exists('exam-12345678_soln.pdf'))
+    def test_build2(self):
+        c=Config('exam_description5.yaml')
+        self.assertEqual(len(c.serials),2)
+        for serial in c.serials:
+            keymap=dict(serial=serial)
+            c.document.resolve_instance(keymap)
+            c.LB.build_document(c.document,make_solutions=True)
+            self.assertTrue(os.path.exists(f'exam-{serial}.pdf'))
+            self.assertTrue(os.path.exists(f'exam-{serial}_soln.pdf'))
