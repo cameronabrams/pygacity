f7da3aef4fab166efa7cecb931d067c9cf21cf3c
Cameron Abrams <cfa22@drexel.edu>
2025-01-13 16:59:37 -0500

    tests

--- tests/unit/test_pick.py
+++ tests/unit/test_pick.py
@@ -1,7 +1,5 @@
 import unittest
-import os
 from thermoproblems.pick import Picker
-import yaml
 
 class PickTest(unittest.TestCase):
     def test_picker1(self):
@@ -11,8 +9,8 @@ class PickTest(unittest.TestCase):
                             'Psat': {'pick':{'between':[2.7,2.9],'round':3}},
                             'TLC':  {'pick':{'between':[45,55],'round':0}},
                             'mdot': {'pick':{'between':[10,50],'round':0}}})
-        self.assertTrue(state['P1']>=2.9)
-        self.assertTrue(state['P1']<=3.9)
+        self.assertTrue(state.P1>=2.9)
+        self.assertTrue(state.P1<=3.9)
     def test_picker2(self):
         P=Picker(12345678)
         state=P.pick_state({'P1':   {'pick':{'pickfrom':[2.9,3.1,3.3,3.5,3.7,3.9]}},
@@ -20,4 +18,8 @@ class PickTest(unittest.TestCase):
                             'Psat': {'pick':{'between':[2.7,2.9],'round':3}},
                             'TLC':  {'pick':{'between':[45,55],'round':0}},
                             'mdot': {'pick':{'between':[10,50],'round':0}}})
-        self.assertTrue(state['P1'] in [2.9,3.1,3.3,3.5,3.7,3.9])
+        self.assertTrue(state.P1 in [2.9,3.1,3.3,3.5,3.7,3.9])
+    def test_picker_defaults(self):
+        P=Picker(0)
+        state=P.pick_state(dict(P={'default':10.0,'pick':{'between':[9,11],'round':2}}))
+        self.assertEqual(state.P,10.0)
--- thermoproblems/pick.py
+++ thermoproblems/pick.py
@@ -1,33 +1,37 @@
 # Author: Cameron F. Abrams, <cfa22@drexel.edu>
 import numpy as np
+from argparse import Namespace
 
 class Picker:
     def __init__(self,serial=0):
-        self.rng=np.random.default_rng(serial)
+        self.rng=np.random.default_rng(serial) if serial!=0 else None
     def pick_state(self,specs):
         _pick_recursive(specs,self.rng)
-        return specs
+        return Namespace(**specs)
     
-
 def _pick_recursive(specs,rng):
     if not type(specs)==dict:
         return 
-    for k,v in specs.items():
+    for k,v in specs.items():        
         if type(v)==dict and 'pick' in v:
             pickrule=v['pick']
-            if 'between' in pickrule:
-                lims=pickrule['between']
-                r=rng.random()
-                specs[k]=lims[0]+r*(lims[1]-lims[0])
-                if 'round' in pickrule:
-                    specs[k]=np.round(specs[k],pickrule['round'])
-            elif 'pickfrom' in pickrule:
-                domain=pickrule['pickfrom']
-                specs[k]=rng.choice(domain)
-                if 'round' in pickrule:
-                    specs[k]=np.round(specs[k],pickrule['round'])
+            if rng==None:
+                assert 'default' in v,f'Error: serial is 0 but no default for {k} is given'
+                specs[k]=v['default']
             else:
-                raise Exception('Missing picking rule')
+                if 'between' in pickrule:
+                    lims=pickrule['between']
+                    r=rng.random()
+                    specs[k]=lims[0]+r*(lims[1]-lims[0])
+                    if 'round' in pickrule:
+                        specs[k]=np.round(specs[k],pickrule['round'])
+                elif 'pickfrom' in pickrule:
+                    domain=pickrule['pickfrom']
+                    specs[k]=rng.choice(domain)
+                    if 'round' in pickrule:
+                        specs[k]=np.round(specs[k],pickrule['round'])
+                else:
+                    raise Exception('Missing picking rule')
         else:
             _pick_recursive(v,rng)
         
