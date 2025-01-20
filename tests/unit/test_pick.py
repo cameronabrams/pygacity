17b5c3ee5b1d5013893dd1c0549ecff1afe0e18c
Cameron Abrams <cfa22@drexel.edu>
2025-01-20 14:31:25 -0500

    pick stepper

--- tests/unit/test_pick.py
+++ tests/unit/test_pick.py
@@ -1,5 +1,8 @@
 import unittest
-from thermoproblems.pick import Picker
+from thermoproblems.pick import Picker, Stepper
+import numpy as np
+from itertools import product
+from argparse import Namespace
 
 class PickTest(unittest.TestCase):
     def test_picker1(self):
@@ -23,3 +26,39 @@ class PickTest(unittest.TestCase):
         P=Picker(0)
         state=P.pick_state(dict(P={'default':10.0,'pick':{'between':[9,11],'round':2}}))
         self.assertEqual(state.P,10.0)
+    def test_stepper0(self):
+        S=Stepper({'P1':   {'pick':{'between':[2.9,3.9],'round':1}},
+                            'T1C':  {'pick':{'between':[340,360],'round':0,'intervals':11}},
+                            'Psat': {'pick':{'between':[2.7,2.9],'round':3}},
+                            'TLC':  {'pick':{'between':[45,55],'round':0}},
+                            'mdot': {'pick':{'between':[10,50],'round':0}}})
+        self.assertEqual(len(S.space.P1),10)
+        self.assertTrue(np.all(S.space.P1==np.linspace(2.9,3.9,10)))
+        allv=[k for k in vars(S.space).values()]
+        self.assertTrue(np.all(allv[1]==np.linspace(340,360,11)))
+        T=product(*allv)
+        TT=list(T)
+        self.assertEqual(len(TT),10*11*10*10*10)
+        T=product(*allv)
+        t=next(T)
+        resdict={k:v for k,v in zip(vars(S.space).keys(),t)}
+        X=Namespace(**resdict)
+        
+        self.assertEqual(X.P1,2.9)
+        self.assertEqual(X.T1C,340)
+        self.assertEqual(X.Psat,2.7)
+        self.assertEqual(X.TLC,45)
+        self.assertEqual(X.mdot,10)
+        self.assertEqual(len(t),5)
+    def test_stepper1(self):
+        S=Stepper({'P1':   {'pick':{'between':[2.9,3.9],'round':1}},
+                            'T1C':  {'pick':{'between':[340,360],'round':0,'intervals':11}},
+                            'Psat': {'pick':{'between':[2.7,2.9],'round':3}},
+                            'TLC':  {'pick':{'between':[45,55],'round':0}},
+                            'mdot': {'pick':{'between':[10,50],'round':0}}})
+        xx=np.linspace(10,50,10)
+        for i,N in enumerate(S):
+            if i>9: break
+            self.assertEqual(N.P1,2.9)
+            self.assertEqual(N.TLC,45)
+            self.assertEqual(N.mdot,xx[i])
--- thermoproblems/pick.py
+++ thermoproblems/pick.py
@@ -1,14 +1,32 @@
 # Author: Cameron F. Abrams, <cfa22@drexel.edu>
 import numpy as np
 from argparse import Namespace
+from itertools import product
 
 class Picker:
     def __init__(self,serial=0):
         self.rng=np.random.default_rng(serial) if serial!=0 else None
     def pick_state(self,specs):
+        # given the single instance of specs, return a single randomly picked state
         _pick_recursive(specs,self.rng)
         return Namespace(**specs)
-    
+
+class Stepper:
+    def __init__(self,specs):
+        _space_recursive(specs)
+        self.space=Namespace(**specs)
+        allv=[k for k in vars(self.space).values()]
+        self.T=product(*allv)
+    def __iter__(self):
+        return self
+    def __next__(self):
+        try:
+            t=next(self.T)
+            resdict={k:v for k,v in zip(vars(self.space).keys(),t)}
+            return Namespace(**resdict)
+        except:
+            raise StopIteration
+        
 def _pick_recursive(specs,rng):
     if not type(specs)==dict:
         return 
@@ -35,4 +53,21 @@ def _pick_recursive(specs,rng):
         else:
             _pick_recursive(v,rng)
         
+def _space_recursive(specs):
+    if not type(specs)==dict:
+        return 
+    for k,v in specs.items():        
+        if type(v)==dict and 'pick' in v:
+            pickrule=v['pick']
+            if 'between' in pickrule:
+                lims=pickrule['between']
+                intervals=pickrule.get('intervals',10)
+                specs[k]=np.linspace(lims[0],lims[1],intervals)
+            elif 'pickfrom' in pickrule:
+                domain=pickrule['pickfrom']
+                specs[k]=domain
+            else:
+                raise Exception('Missing picking rule')
+        else:
+            _space_recursive(v)
 
